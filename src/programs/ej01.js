2cf8f42cd68ae8adc0aecd691d4716b25a9df7ce
49319314@est.ort.edu.ar <49319314@est.ort.edu.ar>
2026-04-06 09:17:13 -0300

    estable

--- src/programs/ej01.js
+++ src/programs/ej01.js
@@ -1,6 +1,6 @@
 //import productos from "./src/data/productos.json" with {type:"json"}
 import fs from "fs/promises"
 
-let content= JSON.parse(await fs.readFile("../data/productos.json", "utf-8"))
-
+let content= JSON.parse(await fs.readFile("./src//data/productos.json", "utf-8"))
+console.log(content)
 //

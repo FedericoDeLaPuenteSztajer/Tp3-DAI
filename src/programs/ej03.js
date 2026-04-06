13d8007fca7904c298a6906290bd1cf2ab40b8cd
49319314@est.ort.edu.ar <49319314@est.ort.edu.ar>
2026-04-06 10:27:40 -0300

    ej03 terminado

---
+++ src/programs/ej03.js
@@ -0,0 +1,5 @@
+import dayjs from "dayjs"
+//https://day.js.org/docs/es-ES/parse/parse
+
+console.log("Fecha actual: "+ dayjs().hour() +":"+ dayjs().minute())
+console.log("Hora actual: "+ dayjs().day() +":"+ dayjs().month() +":"+ dayjs().year())

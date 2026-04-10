5ee893ecb56228c17f12f93327802de9d56734f3
49319314@est.ort.edu.ar <49319314@est.ort.edu.ar>
2026-04-10 11:44:13 -0300

    tp04 incompleto, no funciona la API, reveer

---
+++ src/programs/ej04.js
@@ -0,0 +1,19 @@
+//CountryStateCity API: https://docs.countrystatecity.in/api/introduction
+//API key: 4017a5d0f21700538866e87a7cf7a2d040cbb70ff201e00e6abf682bfa16f8fa
+
+const response = await fetch('https://api.countrystatecity.in/v1/countries', {
+    headers: { 'X-CSCAPI-KEY': '4017a5d0f21700538866e87a7cf7a2d040cbb70ff201e00e6abf682bfa16f8fa' }
+});
+
+const countries = await response.json();
+
+const idPais1 = 101
+const idPais2 = 233
+
+countries.forEach(country => {
+    if ((country.id == idPais1) || (country.id == idPais2)) {
+        console.log("id: ${country.id}")
+
+    }
+})
+
